1294ef72745fcf365b2aa6f498aea77bb51affa3
Tural Ismayilov <93684732+turalfirst1@users.noreply.github.com>
2021-12-13 23:16:07 +0200

    Day 1 - [ Interactive coding exercise] Printing
    Print function. Its usage.

---
+++ Day_1.py
@@ -0,0 +1,10 @@
+# Printing to the Console in Python
+
+print('Hello World!')
+
+# [Interactive Coding Exercise] Printing
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
