94d25735a63a0129cf014785d27722a33013d749
turalfirst1 <mrturalismayilov@gmail.com>
2021-12-16 00:26:47 +0200

    Day_1 complete

--- Day_1.py
+++ Day_1.py
@@ -1,10 +1,37 @@
-# Printing to the Console in Python
+# # Printing to the Console in Python
+#
+# print('Hello World!')
+#
+# # [Interactive Coding Exercise] Printing
+#
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+#
+# print("Hello World!\nHello World!\nHello World!")
+#
+#
+# print("Hello" + " " + "Tural")
+# print("Hello " + " " + "Tural")
+# print("Hello" + " " + " Tural")
+#
+# # [Interactive coding exercise] Debugging Practice
+#
+# print('Day 1 - String Manupilation')
+# print("String concatenation is done with the '+' sign.")
+# print('e.g.print("Hello World + "Hello World")')
+# print("New lines can be created with a backslash and n")
+#
+# print(input("What is your name? "))
+#
+# print("Hello " + input("What is your name:"))
+#
+#
+# print(len(input("What is your name?: ")))
 
-print('Hello World!')
 
-# [Interactive Coding Exercise] Printing
-
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
-
+greeting = "Welcome to the Band Name Generator."
+print(greeting)
+city = input("What's name of the city you grew up in? ")
+pet = input("What's your pets name? ")
+print("Your band name could be " + city + ' ' + pet)
